9c2dc3b88b8db8c9e9bd86327c4a274a49e6b153
Kevin Risden <compuwizard123@gmail.com>
2011-04-03 16:22:52 -0400

    test balance format and message

--- src/TestPrintBalance.java
+++ src/TestPrintBalance.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Locale;
 
@@ -102,5 +103,26 @@ public class TestPrintBalance {
 		Date date = new Date();
 		PrintBalance obj = new PrintBalance("de","DE", date, 9876543.21);
 		assertEquals(obj.printDate(), "Wie der : " + DateFormat.getDateInstance(DateFormat.SHORT, new Locale("de", "DE")).format(date));
+	}
+	
+	@Test
+	public void testPrintBalanceBalanceenUS() {
+		Double balance = 9876543.21;
+		PrintBalance obj = new PrintBalance(new Date(), balance);
+		assertEquals(obj.printBalance(), "You owe the school " + NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(balance));
+	}
+	
+	@Test
+	public void testPrintBalanceBalancefrFR() {
+		Double balance = 9876543.21;
+		PrintBalance obj = new PrintBalance("fr", "FR", new Date(), balance);
+		assertEquals(obj.printBalance(), "Vous devez l'école " + NumberFormat.getCurrencyInstance(new Locale("fr", "FR")).format(balance));
+	}
+	
+	@Test
+	public void testPrintBalanceBalancedeDE() {
+		Double balance = 9876543.21;
+		PrintBalance obj = new PrintBalance("de","DE",new Date(), balance);
+		assertEquals(obj.printBalance(), "Du schuldest der Schule " + NumberFormat.getCurrencyInstance(new Locale("de", "DE")).format(balance));
 	}
 }
