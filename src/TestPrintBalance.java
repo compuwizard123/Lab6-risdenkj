b7fe254e205d1050385524ba91074b59a99fc29b
Kevin Risden <compuwizard123@gmail.com>
2011-04-03 15:53:07 -0400

    test greetings

--- src/TestPrintBalance.java
+++ src/TestPrintBalance.java
@@ -1,5 +1,8 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -13,13 +16,29 @@ public class TestPrintBalance {
 	
 	@Test
 	public void testPrintBalanceDefaultConstructor() {
-		assertNotNull(new PrintBalance());
+		assertNotNull(new PrintBalance(new Date(), 9876543.21));
 	}
 
 	@Test
 	public void testPrintBalanceLocaleConstructor() {
-		assertNotNull(new PrintBalance("en", "US"));
+		assertNotNull(new PrintBalance("en", "US", new Date(), 9876543.21));
 	}
 	
-
+	@Test
+	public void testPrintBalanceGreetingenUS() {
+		PrintBalance obj = new PrintBalance(new Date(), 9876543.21);
+		assertEquals(obj.printGreeting(), "Hello World");
+	}
+	
+	@Test
+	public void testPrintBalanceGreetingfrFR() {
+		PrintBalance obj = new PrintBalance("fr", "FR", new Date(), 9876543.21);
+		assertEquals(obj.printGreeting(), "Bonjour tout le monde");
+	}
+	
+	@Test
+	public void testPrintBalanceGreetingdeDE() {
+		PrintBalance obj = new PrintBalance("de","DE",new Date(), 9876543.21);
+		assertEquals(obj.printGreeting(), "Hallo Welt");
+	}
 }
