0d2f1acbf3fbc47ff79cd3a6175ff5efc6f7d0d4
Kevin Risden <compuwizard123@gmail.com>
2011-04-03 16:12:24 -0400

    test user prompt and user greeting

--- src/TestPrintBalance.java
+++ src/TestPrintBalance.java
@@ -41,4 +41,43 @@ public class TestPrintBalance {
 		PrintBalance obj = new PrintBalance("de","DE",new Date(), 9876543.21);
 		assertEquals(obj.printGreeting(), "Hallo Welt");
 	}
+	
+	@Test
+	public void testPrintBalanceUserPromptenUS() {
+		PrintBalance obj = new PrintBalance(new Date(), 9876543.21);
+		assertEquals(obj.printUserPrompt(), "Please enter your name");
+	}
+	
+	@Test
+	public void testPrintBalanceUserPromptfrFR() {
+		PrintBalance obj = new PrintBalance("fr", "FR", new Date(), 9876543.21);
+		assertEquals(obj.printUserPrompt(), "S'il vous plaît entrer votre nom");
+	}
+	
+	@Test
+	public void testPrintBalanceUserPromptdeDE() {
+		PrintBalance obj = new PrintBalance("de","DE",new Date(), 9876543.21);
+		assertEquals(obj.printUserPrompt(), "Bitte geben Sie Ihren Namen");
+	}
+	
+	@Test
+	public void testPrintBalanceUserGreetingenUS() {
+		PrintBalance obj = new PrintBalance(new Date(), 9876543.21);
+		String name = "kevin";
+		assertEquals(obj.printUserGreeting(name), "I am pleased to meet you " + name);
+	}
+	
+	@Test
+	public void testPrintBalanceUserGreetingfrFR() {
+		PrintBalance obj = new PrintBalance("fr", "FR", new Date(), 9876543.21);
+		String name = "kevin";
+		assertEquals(obj.printUserGreeting(name), "Je suis heureux de vous rencontrer " + name);
+	}
+	
+	@Test
+	public void testPrintBalanceUserGreetingdeDE() {
+		PrintBalance obj = new PrintBalance("de","DE",new Date(), 9876543.21);
+		String name = "kevin";
+		assertEquals(obj.printUserGreeting(name), "Ich freue mich, Sie kennen zu lernen " + name);
+	}
 }
